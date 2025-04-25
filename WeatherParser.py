59ab0143d8bb3e14525ef734e0b378ef9bfe08be
KyMiR4iK <bystr.mark03@gmail.com>
2025-04-25 16:27:51 +0300

    Создания класса WeatherParser и его основных методов для парсинга, сохранения и показа информации.

---
+++ WeatherParser.py
@@ -0,0 +1,12 @@
+class WeatherParser:
+
+    def __init__(self): # конструктор
+        self.web_page = "123" # Сайт для парсинга.
+
+    def parse_page(self, web_page):
+        pass    
+    
+    def get_temperature(self, souped_page):
+        pass
+
+    
